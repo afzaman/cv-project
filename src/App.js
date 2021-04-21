41497aa2d043cf233f2e5aa8c19a7bf05a71737e
afzaman <afzaman2003@gmail.com>
2021-04-21 13:48:24 -0400

    Added styling from bootstrap

--- src/App.js
+++ src/App.js
@@ -3,51 +3,98 @@ import { TextInput } from "./components/TextInput"
 
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      educationInputs: [],
+      experienceInputs: []
+    }
+  }
+
+  addEducation = (inputs) => {
+    this.setState({
+      educationInputs: this.state.educationInputs.concat(inputs)
+    })
+  }
+
+  addExperience = (inputs) => {
+    this.setState({
+      experienceInputs: this.state.experienceInputs.concat(inputs)
+    })
+  }
+
+  removeExperience = () => {
+    this.setState({
+      experienceInputs: this.state.experienceInputs.slice(0,-1)
+    })
+  }
+
+  removeEducation = () => {
+    this.setState({
+      educationInputs: this.state.educationInputs.slice(0,-1)
+    })
+  }
   
   render = () => {
 
     const personalInputs = [
-        "First Name",
-        "Last Name",
-        "Address",
-        "Phone Number",
-        "Email",
-        "Description"
+      "First Name",
+      "Last Name",
+      "Address",
+      "Phone Number",
+      "Email",
+      "Description"
     ]
 
-    const educationInputs = [
-      "Degree Type",
-      "Major",
-      "School",
-      "City, State",
-      "Start Year",
-      "End Year"
-    ]
+    const experience = {
+      company : "Company Name",
+      startYear : "Start Year",
+      endYear : "End Year",
+      role : "Role",
+      description : "Description",
+      location : "City, State"
+    }
 
-    const experienceInputs = [
-      "Degree Type",
-      "Major",
-      "School",
-      "City, State",
-      "Start Year",
-      "End Year"
-    ]
+    const education = {
+      degree : "Degree Type",
+      major : "Major",
+      school : "School",
+      location : "City, State",
+      startYear : "Start Year",
+      endYear : "End Year"
+    }
 
     return (
-      <div>
-          <div>
-            <h3>Personal Information</h3>
+      <div className="container">
+        <div className="navbar nav"> 
+          <h1>My Curriculum Vitae</h1>
+        </div>
+          <div className="row">
+            <h3 className="p-3 border bg-light">Personal Information</h3>
             {personalInputs.map((input) => <TextInput placeholder={input}/>)}
           </div>
-          <div>
-            <h3>Education</h3>
-            {educationInputs.map((input) => <TextInput placeholder={input}/>)}
-            <button>Add Another</button>
+          
+          <div className="row">
+            <h3 className="p-3 border bg-light">Education</h3>
+            {this.state.educationInputs.map((education) => 
+              Object.keys(education).map((input, i) => 
+                <TextInput placeholder={education[input]}/>
+            ))}
+            <div className="col-sm-4 p-3">
+              <button className="btn btn-primary m-2" onClick={(() => this.addEducation(education))}>Add Another</button>
+              <button className="btn btn-danger m-2" onClick={this.removeEducation.bind(this)}>Remove</button>
+            </div>
           </div>
-          <div>
-            <h3>Work History</h3>
-            {experienceInputs.map((input) => <TextInput placeholder={input}/>)}
-            <button>Add Another</button>
+
+          <div className="row">
+            <h3 className="p-3 border bg-light">Work History</h3>
+            {this.state.experienceInputs.map((experience) => Object.keys(experience).map((input, i) => 
+              <TextInput placeholder={experience[input]}/>
+            ))}
+            <div className="col-sm p-3">
+              <button className="btn btn-primary m-2" onClick={(() => this.addExperience(experience))}>Add Another</button>
+              <button className="btn btn-danger m-2" onClick={this.removeExperience.bind(this)}>Remove</button>
+            </div>
           </div>
       </div>
     );
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ContentEditable from 'react-contenteditable'
+import uniqid from 'uniqid'
 
 export class TextInput extends React.Component{
 
@@ -8,23 +9,33 @@ export class TextInput extends React.Component{
         this.contentEditable = React.createRef()
         this.state = {
           html: props.placeholder,
+          key: null
         }
       }
     
     handleChange = e => {
         this.setState({
-          html: e.target.value
+          html: e.target.value,
+          key: uniqid()
         })
       }
 
+    handlesubmit = e => {
+      this.setState({
+        key: uniqid()
+      })
+    }
+
     render = () => {
         return(
-            <div>
+            <div className="col-lg-4">
                 <ContentEditable
                     innerRef={this.contentEditable}
                     html={this.state.html}
                     disabled={false}
                     onChange={this.handleChange}
+                    key={this.state.key}
+                    onSubmit={this.handlesubmit}
                 /> 
             </div>
         )
